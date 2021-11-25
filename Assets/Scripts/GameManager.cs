a8f7b2b733531969b26481adf99b3d9bd0b7afb5
FANGCHERWEI <s10206264@connect.np.edu.sg>
2021-11-25 15:24:38 +0800

    ..

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
         Score = 0;
         scoreText.text = "Score : " + Score.ToString();
         panel.SetActive(false);
+        lastpanel.SetActive(false);
     }
 
     public void firstimagetarget()
@@ -40,7 +41,7 @@ public class GameManager : MonoBehaviour
         if (imagetarget[1].activeInHierarchy == true)
         {
 
-            infos.text = "It is important to keep a distance away from one another as it helps to prevent spread of viruses ";
+            infos.text = " It is important to put on the mask  To prevent the spread of covid 19  . keep your mask on";
 
             panel.SetActive(true);
             Score += 10;
